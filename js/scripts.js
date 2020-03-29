6a9ba238b06d39813bbbee4143d52cf05555926b
Daniel Kibby <daniel.kibby@live.co.uk>
2020-03-29 17:05:49 -0500

    v1.6

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(function(){
+    $("#release-agree1").click(function(){
+        let agree1 = true;
+        $('#release1').modal("hide");
+        $('#registerModal1').modal("show");
+    });
+});
+
+$(function(){
+    $("#release-agree2").click(function(){
+        let agree2 = true;
+        $('#release2').modal("hide");
+        $('#registerModal2').modal("show");
+    });
+});
+
+$(function(){
+    $("#release-agree3").click(function(){
+        let agree3 = true;
+        $('#release3').modal("hide");
+        $('#registerModal3').modal("show");
+    });
+});
+
